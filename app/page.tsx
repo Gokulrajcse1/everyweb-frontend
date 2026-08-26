bc527773154f46366b7cac28476ff60141d96b84
Gokulraj <ngokulraj444@gmail.com>
2026-08-26 23:21:07 +0530

    footer ,nav moved to layout page

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,14 @@
-import Navbar from "@/components/Navbar";
 import Hero from "@/components/Hero";
 import Services from "@/components/Services";
 import About from "@/components/About";
 import Chooseus from "@/components/whychooseus";
-import Footer from "@/components/Footer";
 export default function Home() {
   return (
     <main className=" bg-[#efefef]">
-      <Navbar />
       <Hero />
       <Services />
       <About />
       <Chooseus />
-      <Footer />
     </main>
   );
 }
